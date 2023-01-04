3f171c5abf32276f1df36ef99a305ebb63c5e733
xjnL1S <lizak102002@gmail.com>
2023-01-04 03:36:06 +0300

    adding multithreading

--- FileFinderC++/FileFinderCplusplus.cpp
+++ FileFinderC++/FileFinderCplusplus.cpp
@@ -4,15 +4,17 @@
 #include <windows.h>
 #include <cstdio>
 #include <string>
-#include <queue>
 #include "TreeHolder.cpp"
 using namespace std;
 
-static int DEFAULT_MAX_THREADS = 10;
+static int MAX_THREADS = 10;
+static int RUNNING_PARALLEL_THREADS = 0;
 bool isFounded = false;
+
 void findFilesAndCatalogs(const std::string &basePath, std::vector<std::string> &files, std::vector<std::string> &catalogs);
 void buildTreeRecursive(TreeHolder &tree);
-void searchFileInTreeRecursive(TreeHolder &tree, string &searchedFileName);
+void searchFileInTreeRecursive(TreeHolder &tree, string&searchedFileName);
+void startThreadSearching(TreeHolder& tree, string &searchedFileName);
 
 
 int main(int argc, char* argv[]) {
@@ -23,8 +25,7 @@ int main(int argc, char* argv[]) {
 
     std::string basePath = "C:";
 
-    int threads = DEFAULT_MAX_THREADS;
-
+    // Проверяем на наличие обязательных и опциональных параметров
     if (argc == 2 || argc == 4 || argc == 6) {
         searchedFile = argv[1];
         for (int i = 2; i < argc; i += 2) {
@@ -34,7 +35,7 @@ int main(int argc, char* argv[]) {
                 basePath = parameterValue;
             }
             else if (parameterName == "--num_threads") {
-                threads = stoi(parameterValue) <= DEFAULT_MAX_THREADS ? std::stoi(parameterValue) : DEFAULT_MAX_THREADS;
+                MAX_THREADS = stoi(parameterValue) <= MAX_THREADS ? std::stoi(parameterValue) : MAX_THREADS;
             }
             else {
                 cout << "Only parameters --path and --num_threads supported";
@@ -43,16 +44,24 @@ int main(int argc, char* argv[]) {
         }
     }
     else {
-        cout << "Please enter filename as required first parameter. Also you can give optional parameters like this o./find_app some_file_name --path base_path --num_threads 15";
+        cout << "Please enter filename as required first parameter. Also you can give optional parameters like this o./find_app some_file_name --path base_path --num_threads 8";
         return -1;
     }
 
+    //Создаем стартовый заданный корень дерева и выстраиваем рекурсивно узлы дерева
     TreeHolder startTree;
     startTree.setPath(basePath);
     std::cout << "Starting building tree from: " + basePath << std::endl;
     buildTreeRecursive(startTree);
     std::cout << "Building tree is finished" << std::endl;
+    std::cout << "Search file: " + searchedFile << std::endl;
+    std::cout << "Max threads: " << MAX_THREADS << std::endl;
+
     searchFileInTreeRecursive(startTree, searchedFile);
+
+    while (RUNNING_PARALLEL_THREADS > 0) {
+        this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
     if (!isFounded) {
         std::cout << "Can't find this file: " + searchedFile << std::endl;
@@ -82,6 +91,7 @@ void findFilesAndCatalogs(const std::string& basePath, std::vector<std::string>&
     }
 };
 
+//функция по рекурсивному построению дерева
 void buildTreeRecursive(TreeHolder &tree) {
     vector<string> files;
     vector<string> catalogs;
@@ -90,6 +100,7 @@ void buildTreeRecursive(TreeHolder &tree) {
 
     tree.setContainedFiles(files);
     
+    //Если были найдены каталоги в дереве, то создаем дочерние узлы дерева и вызываем рекурсию еще раз
     for (string child : catalogs) {
          TreeHolder treeChild;
          treeChild.setPath(tree.getPath() + "/" + child);
@@ -98,23 +109,42 @@ void buildTreeRecursive(TreeHolder &tree) {
     }
 }
 
-void searchFileInTreeRecursive(TreeHolder& tree, string& searchedFileName) {
-        if (!tree.getContainedFiles().empty()) {
-            for (string file : tree.getContainedFiles()) {
-                if (file == searchedFileName) {
-                    std::cout << "Founded file in path: " + tree.getPath() + "/" + searchedFileName << std::endl;
-                    isFounded = true;
-                    return;
-                }
-            }
+//функция по рекурсивному поиску файлов
+void searchFileInTreeRecursive(TreeHolder &tree, string &searchedFileName) {
+    if (isFounded) {
+        return;
+    }
+    for (string file : tree.getContainedFiles()) {
+        if (file == searchedFileName) {
+           std::cout << "Founded file in path: " + tree.getPath() + "/" + searchedFileName << std::endl;
+           isFounded = true;
+           return;
         }
-        for (TreeHolder childTree : tree.getChildsTreeHolders()) {
-            if (!isFounded) {
+    }
+
+    // Для каждого дочернего узла подключаем при поиске (в случае наличия свободных) новый фоновый поток
+    for (TreeHolder childTree : tree.getChildsTreeHolders()) {
+        if (!isFounded) {
+            if (RUNNING_PARALLEL_THREADS < MAX_THREADS) {
+                thread thread(startThreadSearching, ref(tree), ref(searchedFileName));
+                thread.detach();
+                
+                //ждем старта параллельного потока (чтобы в основном потоке счетчик работающих параллельных потоков не стал 0 раньше, чем закончится работа параллельных потоков).
+                this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+            else {
                 searchFileInTreeRecursive(childTree, searchedFileName);
+            }
         }
     }
 }
 
+//функция по старту потока (инкремент, декремент счетчика потоков)
+void startThreadSearching(TreeHolder &tree, string &searchedFileName) {
+    RUNNING_PARALLEL_THREADS++;
+    searchFileInTreeRecursive(tree, searchedFileName);
+    RUNNING_PARALLEL_THREADS--;
+}
 
 
 
